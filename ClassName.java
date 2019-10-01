fc758b29a4729893ca1cda0432e6b62580b6c7fb
franceska.padillacoo <franceska.padillacoo@bishopireton.org>
2019-10-01 12:54:48 -0400

    Added an example file

---
+++ ClassName.java
@@ -0,0 +1,17 @@
+public class ClassName
+{
+   //fields
+   
+   //main method
+   public static void main(String[] args)
+   {
+      //how to print in the command line
+      System.out.println("Hello World");
+   }
+   
+   //method example
+   public static int methodName(int param)
+   {
+      return -1;
+   }
+}
